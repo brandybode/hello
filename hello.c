8394a8fb58e75f6ae9dc92d82a822909d0f06916
Brandy Bode <bbode6@huskers.unl.edu>
2023-08-24 11:13:59 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Brandy Bode
+ *         EMAIL
+ * Date: 2023/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World\n");
+  printf("I love the School of Computing!\n");
+
+  return 0;
+}
